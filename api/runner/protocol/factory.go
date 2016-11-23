1a516ab503c5d27ba4e2f15873e90f045c48504d
Carlos C <uldericofilho@gmail.com>
2016-11-23 21:35:16 +0100

    protocol: make clear that protocols are hot container only

--- api/runner/protocol/factory.go
+++ api/runner/protocol/factory.go
@@ -16,13 +16,18 @@ type ContainerIO interface {
 	Dispatch(stdin io.Reader, stdout io.Writer) error
 }
 
+// Protocol defines all non-default protocols that operates a ContainerIO. If
+// a protocol is being used, then it must be a hot container.
 type Protocol string
 
+// Hot container protocols
 const (
 	HTTP Protocol = models.FormatHTTP
 	JSON Protocol = models.FormatJSON
 )
 
+// New creates a valid protocol handler from a I/O pipe representing containers
+// stdin/stdout.
 func New(p Protocol, in io.Writer, out io.Reader) (ContainerIO, error) {
 	switch p {
 	case HTTP:
